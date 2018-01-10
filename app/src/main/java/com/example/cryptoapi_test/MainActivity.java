822099d4b3f11426af2bc487ba6f138a5f2423a0
liamjg <gallagherl0491@my.uwstout.edu>
2018-01-09 21:26:45 -0600

    WIP

--- app/src/main/java/com/example/cryptoapi_test/MainActivity.java
+++ app/src/main/java/com/example/cryptoapi_test/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.os.Handler;
+import com.google.gson.Gson;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -20,42 +22,52 @@ public class MainActivity extends AppCompatActivity {
     EditText searchText;
     TextView responseView;
     ProgressBar progressBar;
+    boolean update = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        runnable.run();
 
-        responseView = (TextView) findViewById(R.id.responseView);
-        searchText = (EditText) findViewById(R.id.searchText);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
-        Button searchButton = (Button) findViewById(R.id.searchButton);
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new RetrieveFeedTask().execute();
-            }
-        });
-
+//        responseView = (TextView) findViewById(R.id.responseView);
+//        searchText = (EditText) findViewById(R.id.searchText);
+//        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
     }
+
+//    public void parseCryptoCompAPI(String precomp){
+//
+//    }
+
+    private Handler handler = new Handler();
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run(){
+            new RetrieveFeedTask().execute();
+            handler.postDelayed(runnable,1000);
+
+        }
+    };
 
     class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
 
         private Exception exception;
 
         protected void onPreExecute() {
-            progressBar.setVisibility(View.VISIBLE);
-            responseView.setText("");
+//            progressBar.setVisibility(View.VISIBLE);
+//            responseView.setText("");
         }
 
         @Override
         protected String doInBackground(Void... urls) {
-            String coinType = searchText.getText().toString();
+            //String coinType = searchText.getText().toString();
 
             try {
-                URL url = new URL("https://min-api.cryptocompare.com/data/price?fsym=" + coinType + "&tsyms=BTC,USD,EUR");
+                URL url = new URL("https://min-api.cryptocompare.com/data/price?fsym=" + "LTC" + "&tsyms=BTC,USD,EUR");
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 try {
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
@@ -79,9 +91,11 @@ public class MainActivity extends AppCompatActivity {
             if (response == null) {
                 response = "ERROR";
             }
-            progressBar.setVisibility(View.GONE);
+           // progressBar.setVisibility(View.GONE);
             Log.i("INFO", response);
-            responseView.setText(response);
+           // responseView.setText(response);
+
+         //   parseCryptoCompAPI(response);
         }
     }
 }
